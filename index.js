41afc2ef137f6d0728b53fa3d83f6c90bc3800fd
Praise-sofoluwe <112418722+Praise-sofoluwe@users.noreply.github.com>
2023-03-24 10:35:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+const primaryNav = document.querySelector(".primary-navigation");
+const nav = document.querySelector(".nav-shadow");
+
+openMenu.addEventListener("click", () => {
+    primaryNav.classList.add("show");
+    nav.classList.add(`shadow`);
+})
+
+closeMenu.addEventListener('click', () => {
+    primaryNav.classList.remove("show");
+    nav.classList.remove("shadow");
+})
